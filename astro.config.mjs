c8f415cb47372096ab707904bb66e3d0179685ba
ANTI-VILLAIN <antivillains@gmail.com>
2024-04-19 12:08:31 -0400

    Added GitHub Deploy Action

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://example.com',
+    site: 'https://anti-villain.com',
     integrations: [
         mdx(),
         sitemap(),
