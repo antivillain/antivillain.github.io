58c6d84a0541756a5a87603b206b133e6b15d781
ANTI-VILLAIN <antivillains@gmail.com>
2025-01-31 11:38:19 -0500

    Updated tag settings, added partytown

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,15 +2,25 @@ import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import tailwind from '@astrojs/tailwind';
+import { defineConfig } from 'astro/config';
+import mdx from '@astrojs/mdx';
+import partytown from '@astrojs/partytown'
+import sitemap from '@astrojs/sitemap';
 
-// https://astro.build/config
+
 export default defineConfig({
     site: 'https://anti-villain.com',
     integrations: [
-        mdx(),
+        mdx(), 
         sitemap(),
+        partytown({
+            config: {
+              forward: ["dataLayer.push"],
+            },
+        }),
         tailwind({
             applyBaseStyles: false
         })
-    ]
+    ],
 });
+
